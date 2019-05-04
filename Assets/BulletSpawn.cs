479bd11d2d1340a3ea84ed30e8d4400ca9e65728
Jason C Ho <jch032@UCSD.EDU>
2019-05-04 12:57:36 -0700

    scene

---
+++ Assets/BulletSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            GameObject bull = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+            bull.GetComponent<Rigidbody>().AddForce(transform.position * 1000);
+        }
+    }
+}
